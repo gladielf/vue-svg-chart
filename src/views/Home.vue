f7225a4c797cdc99ef0ea63251f80e13885bed44
Fermin conejo <gladielf@gmail.com>
2018-12-26 19:53:25 +0100

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .home
+    svg-chart
+</template>
+
+<script>
+// @ is an alias to /src
+import SvgChart from "@/components/svg-chart.vue";
+
+export default {
+  name: "home",
+  components: {
+    SvgChart
+  }
+};
+</script>
