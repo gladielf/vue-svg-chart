19de67252582a10e9c6b1739794121723c471c61
Fermin conejo <gladielf@gmail.com>
2019-05-11 21:55:29 +0200

    Updated lint files

--- src/components/svg-chart.vue
+++ src/components/svg-chart.vue
@@ -30,27 +30,27 @@ export default {
 </script>
 
 <style lang="sass">
-    @function calcu($val)
-        @return 8em * $val / 100
+@function calcu($val)
+    @return 8em * $val / 100
 
-    $list: (calcu(25), calcu(60), calcu(30), calcu(75), calcu(40), calcu(50), calcu(88), calcu(66))
+$list: (calcu(25), calcu(60), calcu(30), calcu(75), calcu(40), calcu(50), calcu(88), calcu(66))
 
-    .chart
-        --pos: 8em
-        $root-node: &
-        background: #ccccfc
+.chart
+    --pos: 8em
+    $root-node: &
+    background: #ccccfc
 
-        &:hover
-          #{$root-node}__group
-            @for $i from 1 through length($list)
-              &:nth-child(#{$i})
-                --pos: #{nth($list, $i)}
-                #{$root-node}__line--mod
-                    transition-delay: .15s * ($i - 1)
+    &:hover
+      #{$root-node}__group
+        @for $i from 1 through length($list)
+          &:nth-child(#{$i})
+            --pos: #{nth($list, $i)}
+            #{$root-node}__line--mod
+                transition-delay: .15s * ($i - 1)
 
-        &__line
-            &--mod
-              stroke-dasharray: 8em 8em
-              stroke-dashoffset: var(--pos)
-              transition: all .25s ease-in-out
+    &__line
+        &--mod
+          stroke-dasharray: 8em 8em
+          stroke-dashoffset: var(--pos)
+          transition: all .25s ease-in-out
 </style>
